f51c74265f8fa6f062f73bccdbc505d0a038af23
loque18 <madridx2010@gmail.com>
2022-11-08 15:40:30 -0500

    chore: update website title

--- src/static/app.config.js
+++ src/static/app.config.js
@@ -1,8 +1,8 @@
 const appConfig = {
-    appName: 'Template',
-    appVersion: '2.1.0',
+    appName: 'Sportlabs',
+    appVersion: '1.0.0',
     description:
-        'A template build with nextJS to bootstrap projects, it also contains a set of tools and pre buit code to help developers.', // The meta description element provides a summary of a page's content that search engines include in search results.
+        'Sporting Labs is a web3 Sports Fantasy brand dedicated to create play-2-win experiences powered by the blockchain.',
 };
 
 export default appConfig;
